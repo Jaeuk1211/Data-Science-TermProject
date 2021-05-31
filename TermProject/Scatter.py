4208c5aed6540b52aaa555571b7d0c568f121cc4
Jaeuk Kim <ksyj2006@naver.com>
2021-05-31 12:30:24 +0900

    scatter plot crime.py에 업데이트

--- TermProject/Scatter.py
+++ TermProject/Scatter.py
@@ -16,31 +16,32 @@ import matplotlib.pyplot as plt
 path = '/Users/jaeuk/Desktop/python/TermProject/Crime.csv'
 df = pd.read_csv(path)
 
-df = df[['Latitude', 'Longitude', 'Crime Name1']]
+# drop the 0 value
+sc = df[['Latitude', 'Longitude', 'Crime Name1']]
+sc = sc[sc['Latitude'] != 0]
+sc = sc[sc['Longitude'] != 0]
 
-df = df[df['Latitude'] != 0]
-df = df[df['Longitude'] != 0]
+# drop the nan value
+sc.dropna(axis=0, inplace=True)
 
-# get the median and fill the null value
-df.dropna(axis=0, inplace=True)
+# Classify Dataframes by the Crime category
+property = sc.copy()
+person = sc.copy()
+society = sc.copy()
+other = sc.copy()
 
-property = df.copy()
-person = df.copy()
-society = df.copy()
-other = df.copy()
+property = sc[sc['Crime Name1'] == 'Crime Against Property']
+person = sc[sc['Crime Name1'] == 'Crime Against Person']
+society = sc[sc['Crime Name1'] == 'Crime Against Society']
+other = sc[sc['Crime Name1'] == 'Other']
 
-property = df[df['Crime Name1'] == 'Crime Against Property']
-person = df[df['Crime Name1'] == 'Crime Against Person']
-society = df[df['Crime Name1'] == 'Crime Against Society']
-other = df[df['Crime Name1'] == 'Other']
-
-
+# plot data size = 1/100
 property = property.loc[:2500,:]
 person = person.loc[:2500,:]
 society = society.loc[:2500,:]
 other = other.loc[:2500,:]
 
-
+# Plot the Scatter
 plt.scatter(property['Latitude'], property['Longitude'], label='property',c='r')
 plt.scatter(person['Latitude'], person['Longitude'], label='person',c='g')
 plt.scatter(society['Latitude'], society['Longitude'], label='society',c='b')
